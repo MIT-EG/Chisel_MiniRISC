53cefcb80183e65ff2cad3e95a256421f1b5965d
Cseh Péter <csehpeti93@gmail.com>
2017-04-09 22:43:24 +0200

    ALU with operations and flags Initial unit test for the add operation

--- src/main/scala/ALU/alu.scala
+++ src/main/scala/ALU/alu.scala
@@ -8,7 +8,7 @@ import chisel3.util.{Cat, Fill, MuxCase}
 //Flag bus declaration
 class Flags extends Bundle
 {
-  val carry = SInt(1.W) //Bool()
+  val carry = Bool() // UInt(1.W)
   val negative = Bool()
   val overflow = Bool()
   val zero = Bool()
@@ -19,8 +19,8 @@ class Alu extends Module
 {
   val io = IO( new Bundle
   {
-    val a       = Input(SInt(Constants.DataWidth.W))
-    val b       = Input(SInt(Constants.DataWidth.W))
+    val a       = Input(UInt(Constants.DataWidth.W))
+    val b       = Input(UInt(Constants.DataWidth.W))
     val op      = Input(UInt(Constants.OperationWidth.W))
     val flagIn  = Input(new Flags)
 
@@ -29,115 +29,150 @@ class Alu extends Module
   })
 
   //***OPERATIONS***
-  val add = io.a +& io.b +& io.flagIn.carry //a + b + carry in; bitkiterjesztéssel
 
-  //Kivonás esetén a kimenő carry bitet negáljuk???
-  val sub = io.a -& io.b -& io.flagIn.carry //a - b - carry in; bitkiterjesztéssel
-
-  val and = io.a & io.b
-  val or = io.a | io.b
-  val xor = io.a ^ io.b
-
-  val rol = Cat(io.a(Constants.DataWidth - 2, 0), io.a(Constants.DataWidth - 1)) //rotálás balra. '1'010_1010 -> 0101_010'1'
-  val ror = Cat(io.a(0), io.a(Constants.DataWidth - 1, 1))  //rotálás jobbra. 1010_101'0' -> '0'101_0101
-
-  val lshl = (io.a << 1)  //logikai balra shift(minden bitet shiftel)
-  val lshr = (io.a >> 1)  //logikai jobbra shift(minden bitet shiftel)
-
-  val ashl = Cat( io.a(Constants.DataWidth - 1), io.a(Constants.DataWidth - 3, 0), 0.U ) //arithmetikai shift, az előjel bit megmarad, LSB-re 0-t shiftel be. 1_010_1010 -> 1_101_0100
-  val ashr = Cat( io.a(Constants.DataWidth - 1), 0.U, io.a(Constants.DataWidth - 2, 1)) //arithmetika shift, az előjel megmarad, MSB-re 0-t shiftel be, 1_010_1010 -> 1_001_0101
-
-  val swp = Cat( io.a((Constants.DataWidth / 2) - 1, 0), io.a(Constants.DataWidth - 1, Constants.DataWidth / 2))
-
-  //val cmp = Kivonás eredményét használhatom??? a flagekhez ugyanaz kell, de a kimenet más
-  //***
+  //*ARITMETICAL*
+  val add_result = io.a +& io.b +& io.flagIn.carry //a + b + carry in; bitkiterjesztéssel
+  val add_carry = add_result(Constants.DataWidth)
+  val add_ovf = (io.a(Constants.DataWidth - 1) & io.b(Constants.DataWidth - 1)) ^ add_result(Constants.DataWidth - 1) // (a and b) xor result
 
 
-  val result = SInt((Constants.DataWidth + 1).W)
+  val sub_result = io.a -& io.b -& io.flagIn.carry //a - b - carry in; bitkiterjesztéssel
+  val sub_carry = !sub_result(Constants.DataWidth) //carry flag negáltja
+  val sub_ovf = (io.a(Constants.DataWidth - 1) & io.b(Constants.DataWidth - 1)) ^ sub_result(Constants.DataWidth - 1)
 
-// MuxCase szerkezettel, default: io.a
+  //*LOGICAL*
+  //Nincs flag állítás
+  val and_result = io.a & io.b
+  val or_result = io.a | io.b
+  val xor_result = io.a ^ io.b
+
+  //*SHIFT, ROTATE*
+  val rol_result = Cat(io.a(Constants.DataWidth - 2, 0), io.a(Constants.DataWidth - 1)) //rotálás balra. '1'010_1010 -> 0101_010'1'
+
+  val ror_result = Cat(io.a(0), io.a(Constants.DataWidth - 1, 1))  //rotálás jobbra. 1010_101'0' -> '0'101_0101
+
+  val lshl_result = (io.a << 1)  //logikai balra shift(minden bitet shiftel)
+  val lshl_carry = io.a(Constants.DataWidth - 1)
+
+  val lshr_result = (io.a >> 1)  //logikai jobbra shift(minden bitet shiftel)
+  val lshr_carry = io.a(0)
+
+  val ashl_result = Cat( io.a(Constants.DataWidth - 1), io.a(Constants.DataWidth - 3, 0), 0.U ) //arithmetikai shift, az előjel bit megmarad, LSB-re 0-t shiftel be. 1_010_1010 -> 1_101_0100
+  val ashl_carry = io.a(Constants.DataWidth - 2)
+
+  val ashr_result = Cat( io.a(Constants.DataWidth - 1), 0.U, io.a(Constants.DataWidth - 2, 1)) //arithmetika shift, az előjel megmarad, MSB-re 0-t shiftel be, 1_010_1010 -> 1_001_0101
+  val ashr_carry = io.a(0)
+
+  //*SWAP*
+  val swp_result = Cat( io.a((Constants.DataWidth / 2) - 1, 0), io.a(Constants.DataWidth - 1, Constants.DataWidth / 2))
+
+  //*COMPARE*
+  val cmp_zero = io.a === io.b
+  val cmp_neg = io.a < io.b
+
+
+  //Result and flags of the operation
+  val result = UInt((Constants.DataWidth + 1).W)
+  val carry = Bool()
+  val overflow = Bool()
+  val negative = Bool()
+  val zero = Bool()
+
+  // Result MUX, default: io.a
    result := MuxCase(io.a, wrapRefArray(Array(
     (io.op === Operations.pass.U(Constants.OperationWidth.W)) -> io.a,
-    (io.op === Operations.add.U(Constants.OperationWidth.W)) -> add(Constants.DataWidth - 1, 0),
-    (io.op === Operations.sub.U(Constants.OperationWidth.W)) -> sub(Constants.DataWidth - 1, 0),
-    (io.op === Operations.and.U(Constants.OperationWidth.W)) -> and,
-    (io.op === Operations.or.U(Constants.OperationWidth.W)) -> or,
-    (io.op === Operations.xor.U(Constants.OperationWidth.W)) -> xor,
-    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> lshl,
-    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> lshr,
-    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> ashl,
-    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> ashr,
-    (io.op === Operations.swp.U(Constants.OperationWidth.W)) -> swp,
-    (io.op === Operations.cmp.U(Constants.OperationWidth.W)) -> sub //???
+    (io.op === Operations.add.U(Constants.OperationWidth.W)) -> add_result(Constants.DataWidth - 1, 0),
+    (io.op === Operations.sub.U(Constants.OperationWidth.W)) -> sub_result(Constants.DataWidth - 1, 0),
+    (io.op === Operations.and.U(Constants.OperationWidth.W)) -> and_result,
+    (io.op === Operations.or.U(Constants.OperationWidth.W)) -> or_result,
+    (io.op === Operations.xor.U(Constants.OperationWidth.W)) -> xor_result,
+    (io.op === Operations.rol.U(Constants.OperationWidth.W)) -> rol_result,
+    (io.op === Operations.ror.U(Constants.OperationWidth.W)) -> ror_result,
+    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> lshl_result,
+    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> lshr_result,
+    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> ashl_result,
+    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> ashr_result,
+    (io.op === Operations.swp.U(Constants.OperationWidth.W)) -> swp_result,
+    (io.op === Operations.cmp.U(Constants.OperationWidth.W)) -> 0.U
   )))
 
+    //Carry MUX, default: 0
+    //Lehet, hogy érdemes lenne csak azokat az eseteket megírni amikor nem default...
+    carry := MuxCase(0.U, wrapRefArray(Array(
+    (io.op === Operations.pass.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.add.U(Constants.OperationWidth.W))  -> add_carry,
+    (io.op === Operations.sub.U(Constants.OperationWidth.W))  -> sub_carry,
+    (io.op === Operations.and.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.or.U(Constants.OperationWidth.W))   -> 0.U,
+    (io.op === Operations.xor.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.rol.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.ror.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> lshl_carry,
+    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> lshr_carry,
+    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> ashl_carry,
+    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> ashr_carry,
+    (io.op === Operations.swp.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.cmp.U(Constants.OperationWidth.W))  -> 0.U
+  )))
+
+  //Overflow MUX, default: 0
+  overflow := MuxCase(0.U, wrapRefArray(Array(
+    (io.op === Operations.pass.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.add.U(Constants.OperationWidth.W))  -> add_ovf,
+    (io.op === Operations.sub.U(Constants.OperationWidth.W))  -> sub_ovf,
+    (io.op === Operations.and.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.or.U(Constants.OperationWidth.W))   -> 0.U,
+    (io.op === Operations.xor.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.rol.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.ror.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.swp.U(Constants.OperationWidth.W))  -> 0.U,
+    (io.op === Operations.cmp.U(Constants.OperationWidth.W))  -> 0.U
+  )))
+
+  //Zero MUX, default: (result === 0.U)
+  zero := MuxCase((result === 0.U), wrapRefArray(Array(
+    (io.op === Operations.pass.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.add.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.sub.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.and.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.or.U(Constants.OperationWidth.W))   -> (result === 0.U),
+    (io.op === Operations.xor.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.rol.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.ror.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> (result === 0.U),
+    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> (result === 0.U),
+    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> (result === 0.U),
+    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> (result === 0.U),
+    (io.op === Operations.swp.U(Constants.OperationWidth.W))  -> (result === 0.U),
+    (io.op === Operations.cmp.U(Constants.OperationWidth.W))  -> cmp_zero
+  )))
+
+  //Negative MUX, default: result(Constants.DataWidth - 1)
+  negative := MuxCase(result(Constants.DataWidth - 1), wrapRefArray(Array(
+    (io.op === Operations.pass.U(Constants.OperationWidth.W)) -> 0.U,
+    (io.op === Operations.add.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.sub.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.and.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.or.U(Constants.OperationWidth.W))   -> result(Constants.DataWidth - 1),
+    (io.op === Operations.xor.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.rol.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.ror.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.lshl.U(Constants.OperationWidth.W)) -> result(Constants.DataWidth - 1),
+    (io.op === Operations.lshr.U(Constants.OperationWidth.W)) -> result(Constants.DataWidth - 1),
+    (io.op === Operations.ashl.U(Constants.OperationWidth.W)) -> result(Constants.DataWidth - 1),
+    (io.op === Operations.ashr.U(Constants.OperationWidth.W)) -> result(Constants.DataWidth - 1),
+    (io.op === Operations.swp.U(Constants.OperationWidth.W))  -> result(Constants.DataWidth - 1),
+    (io.op === Operations.cmp.U(Constants.OperationWidth.W))  -> cmp_neg
+  )))
+
+
   io.y := result(Constants.DataWidth - 1, 0)
-  io.flagOut.carry := result(Constants.DataWidth)
-  io.flagOut.zero := result(Constants.DataWidth - 1, 0).orR //OR bitredukció, megnézi
-  io.flagOut.negative := result(Constants.DataWidth - 1)
-    //io.flagOut.overflow -> Bemeneti operandusok azonos előjelűek és az aritmetikai művelet(add / sub) eredménye különbözik ettől
+  io.flagOut.carry := carry
+  io.flagOut.overflow := overflow
+  io.flagOut.zero := zero
+  io.flagOut.negative := negative
 }
-
-
-
-/*
-abstract class AluIo extends Module { //inkább bundle csak
-  val io = IO(new Bundle {
-    val a = Input(UInt(Constants.DataWidth.W))
-    val b = Input(UInt(Constants.DataWidth.W))
-    val y = Output(UInt(Constants.DataWidth.W))
-
-    val FlagIn = Input(new Flags)
-    val FlagOut = Output(new Flags)
-  })
-}
-
-//Adder
-class ADD extends Module
-{
-  val io = IO( new Bundle
-  {
-    val a = Input(UInt(Constants.DataWidth.W))
-    val b = Input(UInt(Constants.DataWidth.W))
-    val y = Output(UInt(Constants.DataWidth.W))
-
-    val FlagIn = Input(new Flags)
-    val FlagOut = Output(new Flags)
-  })
-
-  val x = io.a +& io.b +& io.FlagIn.carry     //Add with width expansion
-
-  io.FlagOut.carry := x(Constants.DataWidth)  //MSB : carry
-  io.y := x(Constants.DataWidth - 1, 0)       //others : sum
-  //overflow?? negative?? zero??
-}
-
-//Subtract
-class SUB extends Module
-{
-  val io = IO(new Bundle
-  {
-    val a = Input(UInt(Constants.DataWidth.W))
-    val b = Input(UInt(Constants.DataWidth.W))
-    val y = Output(UInt(Constants.DataWidth.W))
-
-    val FlagIn = Input(new Flags)
-    val FlagOut = Output(new Flags)
-  })
-
-  io.y := io.a - io.b
-  //Flag?
-}
-
-class SHL extends AluIo
-{
-  io.y := (io.a << 1)
-  io.FlagOut.carry := io.a(Constants.DataWidth - 1)
-}
-
-class SHR extends AluIo
-{
-  io.y := (io.a >> 1)
-  io.FlagOut.carry := io.a(0) //LSB a carry bitbe kerül?
-}
-*/
--- src/test/scala/AluTest.scala
+++ src/test/scala/AluTest.scala
@@ -1,6 +1,38 @@
-/**
-  * Created by Crimson on 31/03/2017.
-  */
-class AluTest {
 
+import Chisel.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+import ALU.Alu
+import scala.math.pow
+
+class ALUUnitTester(c: Alu) extends PeekPokeTester(c)
+{
+  private val alu = c
+  val max = pow(2, ALU.Constants.DataWidth).toInt
+  for (i <- 1 to max by 1)
+  {
+    for (j <- 1 to max by 1)
+    {
+      poke(alu.io.op, ALU.Operations.add)
+
+      poke(alu.io.a, i)
+      poke(alu.io.b, j)
+      step(1)
+      expect(alu.io.y, i + j)
+    }
+  }
 }
+
+class ALUTester extends ChiselFlatSpec
+{
+  private val backendNames = Array[String]("firrtl") //Ez mi?
+  for ( backendName <- backendNames )
+  {
+    "Alu" should s"calculate the sum of it's 'a' and 'b' inputs (with $backendName)" in
+      {
+      Driver(() => new Alu, backendName)
+      {
+        c => new ALUUnitTester(c)
+      } should be (true)
+    }
+  }
+}
+
